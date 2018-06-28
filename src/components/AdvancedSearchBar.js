f23ff9202b261e29ee9ce91ad30b98a9677ad80a
prodionov <pavelrodionov@hotmail.com>
2018-06-28 16:08:15 +0100

    changed order for select buttons in search and added first css

--- src/components/AdvancedSearchBar.js
+++ src/components/AdvancedSearchBar.js
@@ -5,7 +5,7 @@ export default class Advanced extends Component {
   state = {
     search: "",
     by_bags_last_30_days: false,
-    featured: true
+    featured: ""
   };
 
   handleChange = event => {
@@ -34,39 +34,44 @@ export default class Advanced extends Component {
     return (
       <div className="searchBar">
         <form>
-          <label>
-            City:
-            <input
-              name="city"
-              value={this.state.search}
-              onChange={this.handleChange}
-            />
-          </label>
+          <input
+            name="city"
+            value={this.state.search}
+            onChange={this.handleChange}
+            placeholder="select location ..."
+          />
+          <button onClick={this.submitRequest}>search</button>
           <br />
           <label>
-            Fetured Stash Points
+            Featured Points
             <select
               name="featured"
               value={this.state.featured}
               onChange={this.handleChange}
             >
-              <option value="true">Yes</option>
+              <option value="" selected>
+                None
+              </option>
+              <option value="true" selected>
+                Yes
+              </option>
               <option value="false">No</option>
             </select>
           </label>
           <br />
           <label>
-            Order by popularity
+            By popularity
             <select
               name="by_bags_last_30_days"
               value={this.state.by_bags_last_30_days}
               onChange={this.handleChange}
             >
               <option value="true">Yes</option>
-              <option value="false">No</option>
+              <option value="false" selected>
+                No
+              </option>
             </select>
           </label>
-          <button onClick={this.submitRequest}>search</button>
         </form>
       </div>
     );
--- src/components/MultipleStash.js
+++ src/components/MultipleStash.js
@@ -4,10 +4,10 @@ import { SingleStash } from "./SingleStash";
 export default class MultipleStash extends Component {
   render() {
     return (
-      <div className="stashesList">
+      <div className="stashList">
         {this.props.stashPoints.map((stash, i) => {
           return (
-            <div key={i} className="stash">
+            <div key={i} className="stashItem">
               <SingleStash stash={stash} />
             </div>
           );
