2364707258a64ac10114ad182d53ed62ac2a270e
prodionov <pavelrodionov@hotmail.com>
2018-06-28 17:16:15 +0100

    added images and borders and more styling Reference #19

--- src/components/SingleStash.js
+++ src/components/SingleStash.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 
-export const SingleStash = ({ stash: { name, address, country } }) => {
+export const SingleStash = ({ stash: { name, address, country, photos } }) => {
   return (
-    <a href="#">
-      <h3>Host name: {name}</h3>
-      <p>Adsress: {address}</p>
-      <p>Country: {country}</p>
-    </a>
+    <div className="itemCard">
+      <a href="#">
+        <h3>Host name: {name}</h3>
+        <p>Adsress: {address}</p>
+        <p>Country: {country}</p>
+      </a>
+      <img src={photos[0]} />
+    </div>
   );
 };
