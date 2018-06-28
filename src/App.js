48311a9a055351df89be7adfcd616821b19c3434
prodionov <pavelrodionov@hotmail.com>
2018-06-28 11:36:57 +0100

    added components folder and create MultpipleStash component that would display owners names Reference #3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import MultipleStash from "./components/MultipleStash";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>hello world</h1>
+        <MultipleStash />
       </div>
     );
   }
