8a91c649f1cd14f99b0054873119e371ba006327
prodionov <pavelrodionov@hotmail.com>
2018-06-28 15:10:38 +0100

    added new component MainDisplay and updated App.js Reference #15

--- src/utils/stashFilter.js
+++ src/utils/stashFilter.js
@@ -2,14 +2,25 @@ const path = "https://api-staging.stasher.com/v1/stashpoints";
 
 export const stashFilter = async params => {
   let pathRequest = "";
+  console.log("params", params);
 
   if (!params) {
     pathRequest = path;
   } else {
-    pathRequest = `${path}?city=${params.payload.search}`;
+    pathRequest = `${path}?`;
+    Object.keys(params.payload).forEach((key, index) => {
+      if (params.payload[key]) {
+        pathRequest += `${key}=${params.payload[key]}&`;
+      }
+    });
+    if (pathRequest[pathRequest.length - 1] == "&") {
+      pathRequest = pathRequest.slice(0, pathRequest.length - 1);
+    }
   }
 
+  console.log("pathRequest", pathRequest);
   const res = await fetch(pathRequest);
   const json = await res.json();
+  console.log("json", json);
   return json;
 };
